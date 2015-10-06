f174218ff823a95ed3ce1e96cd8080c170f5995a
Jermaine Kee <godson919@icloud.com>
2015-10-06 15:11:19 -0400

    updates

--- blabber/updates/admin.py
+++ blabber/updates/admin.py
@@ -3,9 +3,12 @@ from .models import Status, Favorite
 
 # Register your models here.
 class StatusAdmin(admin.ModelAdmin):
-    list_display = ['user', 'text']
+    list_display = ['user', 'text', 'posted_at', 'favorite_count']
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ['user', 'status']
 
 
 #Register models here
 admin.site.register(Status, StatusAdmin)
-admin.site.register(Favorite)
+admin.site.register(Favorite, FavoriteAdmin)
