8e6a725ad4c8301cd57d349406834105247e9c77
Jermaine Kee <godson919@icloud.com>
2015-10-06 12:31:09 -0400

    Admin and Favorite

--- blabber/updates/admin.py
+++ blabber/updates/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Status, Favorite
 
 # Register your models here.
+class StatusAdmin(amin.ModelAdmin):
+    list_display = ['user', 'text']
+
+
+#Register models here
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Favorite)
