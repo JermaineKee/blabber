45f9b445525945acd9dcb250678772fa48a35108
Jermaine Kee <godson919@icloud.com>
2015-10-06 13:53:55 -0400

    correction

--- blabber/updates/admin.py
+++ blabber/updates/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Status, Favorite
 
 # Register your models here.
-class StatusAdmin(amin.ModelAdmin):
+class StatusAdmin(admin.ModelAdmin):
     list_display = ['user', 'text']
 
 
